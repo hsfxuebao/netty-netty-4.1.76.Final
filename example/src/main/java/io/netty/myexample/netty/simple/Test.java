e0e9c8510d87435f1c308f010c96b206bd2604f8
haoshaofei <haoshaofei@kuaishou.com>
2022-05-07 11:49:57 +0800

    Netty入门案例（TCP和HTTP代码）

---
+++ example/src/main/java/io/netty/myexample/netty/simple/Test.java
@@ -0,0 +1,9 @@
+package io.netty.myexample.netty.simple;
+
+import io.netty.util.NettyRuntime;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(NettyRuntime.availableProcessors());
+    }
+}
